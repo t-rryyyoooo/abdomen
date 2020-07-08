8942e613ee0bfdf8db9c7bc2c00f1c2359f9e6c6
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2020-07-08 18:17:36 +0900

    Add kfolds

--- pipliner.sh
+++ pipliner.sh
@@ -27,11 +27,6 @@ save_directory="${DATASET_PATH}/segmentation"
 
 readonly MODEL_SAVEPATH=$(eval echo $(cat ${JSON_FILE} | jq -r ".model_savepath"))
 
-readonly TRAIN_LIST=$(cat ${JSON_FILE} | jq -r ".train_list")
-readonly VAL_LIST=$(cat ${JSON_FILE} | jq -r ".val_list")
-readonly TEST_LIST=$(cat ${JSON_FILE} | jq -r ".test_list")
-test_list=(${TEST_LIST// / })
-
 readonly MODULE_NAME=$(cat ${JSON_FILE} | jq -r ".module_name")
 readonly SYSTEM_NAME=$(cat ${JSON_FILE} | jq -r ".system_name")
 readonly CHECKPOINT_NAME=$(cat ${JSON_FILE} | jq -r ".checkpoint_name")
@@ -50,7 +45,6 @@ readonly EXPERIMENT_NAME=$(cat ${JSON_FILE} | jq -r ".experiment_name")
 # Segmentation input
 readonly DATA_DIRECTORY=$(eval echo $(cat ${JSON_FILE} | jq -r ".data_directory"))
 readonly MODEL_NAME=$(eval echo $(cat ${JSON_FILE} | jq -r ".model_name"))
-model="${MODEL_SAVEPATH}/${MODEL_NAME}"
 
 readonly IMAGE_PATCH_SIZE=$(cat ${JSON_FILE} | jq -r ".image_patch_size")
 readonly LABEL_PATCH_SIZE=$(cat ${JSON_FILE} | jq -r ".label_patch_size")
@@ -66,85 +60,105 @@ readonly TRUE_NAME=$(cat ${JSON_FILE} | jq -r ".true_name")
 readonly PREDICT_NAME=$(cat ${JSON_FILE} | jq -r ".predict_name")
 
 
+readonly TRAIN_LISTS=$(cat ${JSON_FILE} | jq -r ".train_lists")
+readonly VAL_LISTS=$(cat ${JSON_FILE} | jq -r ".val_lists")
+readonly TEST_LISTS=$(cat ${JSON_FILE} | jq -r ".test_lists")
+readonly KEYS=$(cat ${JSON_FILE} | jq -r ".train_lists | keys[]")
 
+all_patients=""
+for key in ${KEYS[@]}
+do 
+ echo $key
+ TRAIN_LIST=$(echo $TRAIN_LISTS | jq -r ".$key")
+ VAL_LIST=$(echo $VAL_LISTS | jq -r ".$key")
+ TEST_LIST=$(echo $TEST_LISTS | jq -r ".$key")
+ test_list=(${TEST_LIST// / })
+ model_savepath="${MODEL_SAVEPATH}/${key}"
+ log="${LOG}/${key}"
+ experiment_name="${EXPERIMENT_NAME}_${key}"
 
-echo "---------- Training ----------"
-echo "Dataset_path:${dataset_path}"
-echo "MODEL_SAVEPATH:${MODEL_SAVEPATH}"
-echo "TRAIN_LIST:${TRAIN_LIST}"
-echo "VAL_LIST:${VAL_LIST}"
-echo "MODULE_NAME:${MODULE_NAME}"
-echo "SYSTEM_NAME:${SYSTEM_NAME}"
-echo "CHECKPOINT_NAME:${CHECKPOINT_NAME}"
-echo "LOG:${LOG}"
-echo "IN_CHANNEL:${IN_CHANNEL}"
-echo "NUM_CLASS:${NUM_CLASS}"
-echo "LEARNING_RATE:${LEARNING_RATE}"
-echo "BATCH_SIZE:${BATCH_SIZE}"
-echo "NUM_WORKERS:${NUM_WORKERS}"
-echo "EPOCH:${EPOCH}"
-echo "GPU_IDS:${GPU_IDS}"
-echo "API_KEY:${API_KEY}"
-echo "PROJECT_NAME:${PROJECT_NAME}"
-echo "EXPERIMENT_NAME:${EXPERIMENT_NAME}"
-
-python3 train.py ${dataset_path} ${MODEL_SAVEPATH} ${MODULE_NAME} ${SYSTEM_NAME} ${CHECKPOINT_NAME} --train_list ${TRAIN_LIST} --val_list ${VAL_LIST} --log ${LOG} --in_channel ${IN_CHANNEL} --num_class ${NUM_CLASS} --lr ${LEARNING_RATE} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --epoch ${EPOCH} --gpu_ids ${GPU_IDS} --api_key ${API_KEY} --project_name ${PROJECT_NAME} --experiment_name ${EXPERIMENT_NAME} 
-
-if [ $? -ne 0 ];then
- exit 1
-fi
-
-echo "---------- Segmentation ----------"
-echo ${test_list[@]}
-for number in ${test_list[@]}
-do
- image="${DATA_DIRECTORY}/case_${number}/${IMAGE_NAME}"
- save="${save_directory}/case_${number}/${SAVE_NAME}"
-
- echo "Image:${image}"
- echo "Model:${model}"
- echo "Save:${save}"
- echo "IMAGE_PATCH_SIZE:${IMAGE_PATCH_SIZE}"
- echo "LABEL_PATCH_SIZE:${LABEL_PATCH_SIZE}"
- echo "OVERLAP:${OVERLAP}"
+ echo "---------- Training ----------"
+ echo "Dataset_path:${dataset_path}"
+ echo "MODEL_SAVEPATH:${model_savepath}"
+ echo "TRAIN_LIST:${TRAIN_LIST}"
+ echo "VAL_LIST:${VAL_LIST}"
+ echo "MODULE_NAME:${MODULE_NAME}"
+ echo "SYSTEM_NAME:${SYSTEM_NAME}"
+ echo "CHECKPOINT_NAME:${CHECKPOINT_NAME}"
+ echo "LOG:${log}"
+ echo "IN_CHANNEL:${IN_CHANNEL}"
+ echo "NUM_CLASS:${NUM_CLASS}"
+ echo "LEARNING_RATE:${LEARNING_RATE}"
+ echo "BATCH_SIZE:${BATCH_SIZE}"
+ echo "NUM_WORKERS:${NUM_WORKERS}"
+ echo "EPOCH:${EPOCH}"
  echo "GPU_IDS:${GPU_IDS}"
+ echo "API_KEY:${API_KEY}"
+ echo "PROJECT_NAME:${PROJECT_NAME}"
+ echo "EXPERIMENT_NAME:${experiment_name}"
 
-
- if [ $MASK_NAME = "No" ];then
-  echo "Mask:${MASK_NAME}"
-  python3 segmentation.py $image $model $save --image_patch_size ${IMAGE_PATCH_SIZE} --label_patch_size ${LABEL_PATCH_SIZE} --overlap $OVERLAP -g ${GPU_IDS}
-
- else
-  mask="${DATA_DIRECTORY}/case_${number}/${MASK_NAME}"
-  echo "Mask:${mask}"
-
-  python3 segmentation.py $image $model $save --mask_path $mask --image_patch_size ${IMAGE_PATCH_SIZE} --label_patch_size ${LABEL_PATCH_SIZE} --overlap $OVERLAP -g ${GPU_IDS}
-
- fi
+ #python3 train.py ${dataset_path} ${model_savepath} ${MODULE_NAME} ${SYSTEM_NAME} ${CHECKPOINT_NAME} --train_list ${TRAIN_LIST} --val_list ${VAL_LIST} --log ${log} --in_channel ${IN_CHANNEL} --num_class ${NUM_CLASS} --lr ${LEARNING_RATE} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --epoch ${EPOCH} --gpu_ids ${GPU_IDS} --api_key ${API_KEY} --project_name ${PROJECT_NAME} --experiment_name ${experiment_name}
 
  if [ $? -ne 0 ];then
   exit 1
  fi
 
+ model="${model_savepath}/${MODEL_NAME}"
+ echo "---------- Segmentation ----------"
+ echo ${test_list[@]}
+ for number in ${test_list[@]}
+ do
+  image="${DATA_DIRECTORY}/case_${number}/${IMAGE_NAME}"
+  save="${save_directory}/case_${number}/${SAVE_NAME}"
+
+  echo "Image:${image}"
+  echo "Model:${model}"
+  echo "Save:${save}"
+  echo "IMAGE_PATCH_SIZE:${IMAGE_PATCH_SIZE}"
+  echo "LABEL_PATCH_SIZE:${LABEL_PATCH_SIZE}"
+  echo "OVERLAP:${OVERLAP}"
+  echo "GPU_IDS:${GPU_IDS}"
+
+
+  if [ $MASK_NAME = "No" ];then
+   echo "Mask:${MASK_NAME}"
+   #python3 segmentation.py $image $model $save --image_patch_size ${IMAGE_PATCH_SIZE} --label_patch_size ${LABEL_PATCH_SIZE} --overlap $OVERLAP -g ${GPU_IDS}
+
+  else
+   mask="${DATA_DIRECTORY}/case_${number}/${MASK_NAME}"
+   echo "Mask:${mask}"
+
+   #python3 segmentation.py $image $model $save --mask_path $mask --image_patch_size ${IMAGE_PATCH_SIZE} --label_patch_size ${LABEL_PATCH_SIZE} --overlap $OVERLAP -g ${GPU_IDS}
+
+  fi
+
+  if [ $? -ne 0 ];then
+   exit 1
+  fi
+
+ done
+
+ all_patients="${all_patients}${TEST_LIST} "
 done
 
-echo "---------- Caluculation ----------"
-echo "TRUE_DIRECTORY:${DATA_DIRECTORY}"
-echo "PREDICT_DIRECTORY:${save_directory}"
-echo "CSV_SAVEPATH:${CSV_SAVEPATH}"
-echo "Test_list:${test_list[@]}"
-echo "NUM_CLASS:${CLASSES}"
-echo "CLASS_LABEL:${CLASS_LABEL}"
-echo "TRUE_NAME:${TRUE_NAME}"
-echo "PREDICT_NAME:${PREDICT_NAME}"
+ echo "---------- Caluculation ----------"
+ echo "TRUE_DIRECTORY:${DATA_DIRECTORY}"
+ echo "PREDICT_DIRECTORY:${save_directory}"
+ echo "CSV_SAVEPATH:${CSV_SAVEPATH}"
+ echo "All_patients:${all_patients[@]}"
+ echo "NUM_CLASS:${CLASSES}"
+ echo "CLASS_LABEL:${CLASS_LABEL}"
+ echo "TRUE_NAME:${TRUE_NAME}"
+ echo "PREDICT_NAME:${PREDICT_NAME}"
 
 
-python3 caluculateDICE.py ${DATA_DIRECTORY} ${save_directory} ${CSV_SAVEPATH} ${TEST_LIST} --classes ${NUM_CLASS} --class_label ${CLASS_LABEL} --true_name ${TRUE_NAME} --predict_name ${PREDICT_NAME} 
+ #python3 caluculateDICE.py ${DATA_DIRECTORY} ${save_directory} ${CSV_SAVEPATH} ${all_patients} --classes ${NUM_CLASS} --class_label ${CLASS_LABEL} --true_name ${TRUE_NAME} --predict_name ${PREDICT_NAME} 
 
-if [ $? -ne 0 ];then
- exit 1
-fi
+ if [ $? -ne 0 ];then
+  exit 1
+ fi
 
-echo "---------- Logging ----------"
-python3 logger.py ${JSON_FILE}
+ echo "---------- Logging ----------"
+ #python3 logger.py ${JSON_FILE}
+
 
