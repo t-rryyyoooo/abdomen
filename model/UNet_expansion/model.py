02e2fb971765b3c14e0ba57294d86f2c22e10ece
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-07-14 08:21:42 +0000

    Finish to make part of UNet

--- model/UNet_expansion/model.py
+++ model/UNet_expansion/model.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from model_part import CreateUpConvBlock, DoubleConvolution
-from pytorch_memlab import profile, MemReporter
 
 class CreateUpConvBlockWithoutZ(nn.Module):
     def __init__(self, in_channel, concat_channel, mid_channel, out_channel, n=2, use_bn=True):
@@ -23,41 +22,37 @@ class PartOfUNet(nn.Module):
     def __init__(self, in_channel_up=128, in_channel_org=64, in_channel_half=128, num_class=14):
         super(PartOfUNet, self).__init__()
 
-        self.expand_2 = CreateUpConvBlockWithoutZ(in_channel_up, in_channel_half, in_channel_half, in_channel_half)
-        self.expand_1 = CreateUpConvBlockWithoutZ(in_channel_half, in_channel_org, in_channel_org, in_channel_org)
+        self.expand_2 = CreateUpConvBlock(in_channel_up, in_channel_half, in_channel_half, in_channel_half)
+        self.expand_1 = CreateUpConvBlock(in_channel_half, in_channel_org, in_channel_org, in_channel_org)
         
         self.segmentation = nn.Conv3d(in_channel_org, num_class, (1, 1, 1), stride=1, dilation=1, padding=(0, 0, 0))
 
         self.softmax = nn.Softmax(dim=1)
 
-    @profile
     def forward(self, input_up, input_org, input_half):
         x = self.expand_2(input_up, input_half)
-        x = self.expand_1(x, input_org)
-        x = self.segmentation(x)
+#x = self.expand_1(x, input_org)
+#x = self.segmentation(x)
         x = self.softmax(x)
 
         return x
 
 
 if __name__ == "__main__":
-    in_channel_up = 64
-    in_channel_org = 32
-    in_channel_half = 64
+    in_channel_up = 128
+    in_channel_org = 64
+    in_channel_half = 128
     model = PartOfUNet(
             in_channel_up=in_channel_up,
             in_channel_org=in_channel_org,
             in_channel_half=in_channel_half,
             num_class=14
             )
-    input_org_shape = [1, in_channel_org, 500, 500, 8]
-    input_half_shape = [1, in_channel_half, 250, 250, 8]
+    input_org_shape = [1, in_channel_org, 500, 500, 8 * 4]
+    input_half_shape = [1, in_channel_half, 250, 250, 8 * 2]
     input_up_shape = [1, in_channel_up, 125, 125, 8]
 
-    torch.backends.cudnn.enabled = False
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    reporter = MemReporter(model)
-    reporter.report()    
     model.to(device)
 
     input_org_dummy = torch.rand(input_org_shape).to(device)
@@ -67,7 +62,4 @@ if __name__ == "__main__":
     print("Device:", device)
     print("Input:", input_org_dummy.shape, input_half_dummy.shape, input_up_dummy.shape)
     output = model(input_up_dummy, input_org_dummy, input_half_dummy)
-    reporter.report()
-
-    print("Output :", output.size())
 
